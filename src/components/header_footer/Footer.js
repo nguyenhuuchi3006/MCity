2b71de3ae47a59ceb7f13bce9936b94bf7e8ab8b
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-19 12:15:38 +0700

    create component icon mancity and adding header, footer

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+import CityLogo from '../ui/icons';
+
 class Footer extends Component {
     render() {
         return (
-            <div>
-                FOOTER
-            </div>
+            <footer className="bck_blue">
+                <div className="footer_logo">
+                    <CityLogo 
+                        width = '70px'
+                        height = '70px'
+                        link = 'true'
+                        linkTo = '/'
+                    />
+                </div>
+
+                <div className="footer_discl">
+                    Manchester city 2019.All rights reserved
+                </div>
+                
+            </footer>
         );
     }
 }
