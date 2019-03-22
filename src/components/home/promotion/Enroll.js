8fe6c259a6fbab71fe974fcacb1fab3f409ab972
Chi Barca <nguyenhuuchi3006@gmail.com>
2019-03-22 13:23:02 +0700

    Create Enroll and lam quen voi form, tao form hien thi bang template

--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Enroll = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import Fade from 'react-reveal/Fade';
+import FormField from '../../ui/formFields';
+
+class Enroll extends Component {
+
+    state ={
+        formError: false,
+        formSuccess: '',
+        formdata:{
+            email:{
+                element: 'input',
+                value: '',
+                config: {
+                    name:'email_input',
+                    type: 'email',
+                    placeholder: 'Enter your email'
+                },
+                validation:{
+                    require: true,
+                    email: true
+                },
+                valid: false,
+                validationMessage: ''
+            }
+        }
+    }
+
+    submitForm(){
+
+    }
+
+    render() {
+        return (
+            <Fade>
+                <div className="enroll_wrapper">
+                    <form onSubmit={(event)=> this.submitForm(event)}></form>
+                
+                    <div className="enroll_title">
+                        enter your email
+                    </div>
+                    <div className="enroll_input">
+                        <FormField 
+                            id={'email'}
+                            formdata={ this.state.formdata.email}
+                            
+                        
+                        />
+                    </div>
+                </div>
+            </Fade>
+        );
+    }
+}
 
 export default Enroll;
